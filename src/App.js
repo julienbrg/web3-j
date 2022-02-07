dcbb2554ff3ba99d13b3fb9374948a663ce23006
julienbrg <julien@strat.cc>
2022-02-07 08:52:52 +0100

    add web3.storage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,45 @@ import { Web3Storage, File } from 'web3.storage/dist/bundle.esm.min.js'
 function App() {
 
   function getAccessToken() {
-    return process.env.WEB3STORAGE_TOKEN;
+    // return process.env.WEB3STORAGE_TOKEN;
+
+    // Get your own API token at https://web3.storage/account/
+    return "12345"; // 
   }
   
   function makeStorageClient() {
+    console.log("makeStorageClient ✅ ");
     return new Web3Storage({ token: getAccessToken() });
   }
 
-  function play() {
-    console.log("Hello! 🎉");
+  function makeFileObjects() {
+    console.log("makeFileObjects ✅ ");
+    // You can create File objects from a Blob of binary data
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    // Here we're just storing a JSON object, but you can store images,
+    // audio, or whatever you want!
+    const obj = { hello: 'world' };
+    const blob = new Blob([JSON.stringify(obj)], {type : 'application/json'});
+  
+    const files = [
+      new File(['contents-of-file-1'], 'plain-utf8.txt'),
+      new File([blob], 'hello.json')
+    ];
+    return files;
+  }
+
+  async function storeFiles(files) {
+    console.log("storeFiles ✅ ");
+    const client = makeStorageClient();
+    const cid = await client.put(files);
+    console.log('stored files with cid:', cid);
+    return cid;
+  }
+
+  async function play() {
+    console.log("Hello! 👋 ");
+    makeStorageClient();
+    storeFiles(makeFileObjects());
   }
 
   return (
